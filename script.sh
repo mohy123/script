84d63bbf42d82d7916392c052abdb810af0629c9
mohy atef <progmohy@gmail.com>
2019-02-04 20:07:03 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 SERVERS="server1_ip server2_ip server3_ip"
-SCRIPT="hostname; df -h ;free -m ;lscpu"
-for SERVERTIP in ${SERVERS};
+COMMANDS="hostname; df -h ;free -m ;lscpu"
+for SERVERIP in ${SERVERS};
 do 
-ssh ${SERVERIP} "SCRIPT" >> output
+ssh ${SERVERIP} "${COMMANDS}" >> output
 done
 less output
 rm output
